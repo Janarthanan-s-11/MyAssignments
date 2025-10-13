6995620ec9ecfa9ef583f7f63d546b9306fdd558
Janarthanan-s-11 <janarthanan110798@gmail.com>
2025-10-13 23:51:57 +0530

    Add files via upload

---
+++ Datatypes.js
@@ -0,0 +1,22 @@
+// Assignment Details:
+// Declare variables using `let` for different data types in JavaScript, and verify their types using `typeof`,
+// including an uninitialized variable.
+
+// Assignment Requirements:
+// userName // String
+// organization // String
+// contactNumber // Number
+// knowsAutomation // Boolean
+// usesPlaywright // Undefined (not assigned)
+
+let userName = 'Janarthanan'
+let organization = 'Agilysys'
+let contactNumber = 8124796647
+let knowsAutomation = true
+let usesPlaywright 
+
+console.log(`The username is ${userName} and the datatype is ${typeof(userName)}`)
+console.log(`The organization is ${organization} and the datatype is ${typeof(organization)}`)
+console.log(`The Contact number is ${contactNumber} and the datatype is ${typeof(contactNumber)}`)
+console.log(`Whether he know automation ${knowsAutomation} and the datatype is ${typeof(knowsAutomation)}`)
+console.log(`He know more than basic of Playwright but stil entire knowledge about is ${typeof(usesPlaywright)}`)
