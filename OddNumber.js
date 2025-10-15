4c766cc82d06a1bd978ccbb269da0ae82411e116
Janarthanan-s-11 <janarthanan110798@gmail.com>
2025-10-15 23:07:01 +0530

    Add files via upload

---
+++ OddNumber.js
@@ -0,0 +1,11 @@
+//Write a JavaScript program that displays all the odd numbers between 1 and 25 on the console.
+
+for(let i= 0; i<=25; i++){
+if(i%2!=0){
+console.log(`The value of i is ${i}, so it's odd number`)
+}
+else{
+console.log(`The value of i is ${i}, so it is even`)    
+}
+
+}
